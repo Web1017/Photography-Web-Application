841b4753f81a311e1c2565e986fb12ed09d54fd9
Javid <j-n_01@hotmail.com>
2019-01-26 12:39:00 -0500

    css changes + app.js, index changes

---
+++ Scripts/app.js
@@ -0,0 +1,12 @@
+// Animate a Smooth Scrooling
+
+$('#view-work').on('click', function () {
+    const images = $('#images').position().top;
+
+    $('html, body').animate({
+
+        scrollTop: images
+
+    }, 900);
+
+});
